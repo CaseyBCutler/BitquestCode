2f11b0dc408966dbd2dda58d9dcb0549247a5146
Casey Cutler <caseybcutler@yahoo.com>
2016-05-03 04:03:37 -0500

    Added files via upload

---
+++ ToggleGameObj.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ToggleGameObj : MonoBehaviour {
+    public GameObject GameObj;
+    //public Text str, stm, vit, intel, agi, spr;
+
+    public void switchOff() {
+        GameObj.SetActive(false);
+        GameInformation.quest[1] = 255;
+        //Debug.Log(str.text);
+        /*GameInformation.Strength = (byte)int.Parse(str.text);
+        Debug.Log(stm.text);
+        GameInformation.Stamina = (byte)int.Parse(stm.text);
+        GameInformation.Vitality = (byte)int.Parse(vit.text);
+        GameInformation.Intelect = (byte)int.Parse(intel.text);
+        GameInformation.Agility = (byte)int.Parse(agi.text);
+        GameInformation.Spirit = (byte)int.Parse(spr.text);*/
+        SaveInformation.SaveNameBioGender();
+        SaveInformation.SaveStatsInformation();
+    }
+    public void switchOn() { GameObj.SetActive(true); }
+}
